146fb18034d769f113fd0eee77d8c9e2ac9ba28b
dimascatur48 <dimascatur48@gmail.com>
2019-10-10 13:43:06 +0700

    init project

---
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.picodiploma.sejiwaproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
