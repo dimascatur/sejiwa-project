771f8fff8cb41eb0ea32e401469e22f99344a124
dimascatur48 <dimascatur48@gmail.com>
2019-10-11 16:11:48 +0700

    create recyclerView with anko layout

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/MainActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/MainActivity.kt
@@ -2,13 +2,28 @@ package com.dicoding.picodiploma.sejiwaproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import org.jetbrains.anko.*
+import org.jetbrains.anko.recyclerview.v7.recyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MainActivityUI().setContentView(this)
     }
 
-    class MainActivityUI
+    class MainActivityUI : AnkoComponent<MainActivity> {
+        override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
+            recyclerView {
+                layoutManager = LinearLayoutManager(context)
+                lparams(width = matchParent, height = matchParent)
+                padding = dip(16)
+                clipToPadding = false
+
+            }
+        }
+    }
 }
