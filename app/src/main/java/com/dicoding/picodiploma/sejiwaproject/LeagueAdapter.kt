fc626feffa2513a8d5489927b5c76f613bafef8c
dimascatur48 <dimascatur48@gmail.com>
2019-10-11 14:35:46 +0700

    Create Adapter

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/LeagueAdapter.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/LeagueAdapter.kt
@@ -1,2 +1,24 @@
 package com.dicoding.picodiploma.sejiwaproject
 
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class LeagueAdapter(private val list: ArrayList<League>) :
+    RecyclerView.Adapter<LeagueAdapter.ListViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
