b64e8767089b3156c0f110f9535a171f6801afd5
dimascatur48 <dimascatur48@gmail.com>
2019-10-15 13:42:00 +0700

    add some function

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/LeagueAdapter.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/LeagueAdapter.kt
@@ -5,6 +5,8 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.dicoding.picodiploma.sejiwaproject.view.ItemLeagueUI
 import org.jetbrains.anko.AnkoContext
 
@@ -15,20 +17,28 @@ class LeagueAdapter(private val list: ArrayList<League>) :
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return list.size
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-//        val club = list[position]
+        val club = list[position]
+
+        Glide.with(holder.itemView.context)
+            .load(club.logo)
+            .apply(RequestOptions().override(80, 80))
+            .into(holder.imgLogo)
+
+        holder.txtName.text = club.name
+        holder.txtStadium.text = club.stadium
+        holder.txtDesc.text = club.description
 
     }
 
     class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val imgLogo: ImageView =  itemView.findViewById(R.id.club_logo) as ImageView
+        val imgLogo: ImageView = itemView.findViewById(R.id.club_logo) as ImageView
         val txtName: TextView = itemView.findViewById(R.id.coach_name) as TextView
         val txtStadium: TextView = itemView.findViewById(R.id.stadium_name) as TextView
         val txtDesc: TextView = itemView.findViewById(R.id.club_desc) as TextView
-
 
 
     }
