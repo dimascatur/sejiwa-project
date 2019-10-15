54b688a65957ff7e15f05fdb23c2f41dcbcaeb5c
dimascatur48 <dimascatur48@gmail.com>
2019-10-15 17:19:25 +0700

    create detail activity

---
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/detail/DetailLeagueActivity.kt
@@ -0,0 +1,13 @@
+package com.dicoding.picodiploma.sejiwaproject.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.picodiploma.sejiwaproject.R
+
+class DetailLeagueActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_league)
+    }
+}
