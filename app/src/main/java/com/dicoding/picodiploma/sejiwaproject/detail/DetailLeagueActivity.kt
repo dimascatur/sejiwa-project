77e8019cdcb097b4aab4dadd5fa9f0b2b7d4eff1
dimascatur48 <dimascatur48@gmail.com>
2019-10-15 19:37:55 +0700

    create some function

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/detail/DetailLeagueActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/detail/DetailLeagueActivity.kt
@@ -2,12 +2,32 @@ package com.dicoding.picodiploma.sejiwaproject.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import com.dicoding.picodiploma.sejiwaproject.model.League
 import com.dicoding.picodiploma.sejiwaproject.R
+import com.dicoding.picodiploma.sejiwaproject.view.DetailLeagueUI
+import org.jetbrains.anko.setContentView
 
 class DetailLeagueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_league)
+        DetailLeagueUI().setContentView(this)
+
+        val imgLogo: ImageView = findViewById(R.id.league_logo)
+        val txtName: TextView = findViewById(R.id.name_league)
+        val txtDesc: TextView = findViewById(R.id.league_desc)
+        val txtLocation: TextView = findViewById(R.id.league_loc)
+
+        val nameLeague = intent.getParcelableExtra<League>(EXTRA_KEY)
+        imgLogo.setImageResource(nameLeague.photo)
+        txtName.text = nameLeague.name
+        txtDesc.text = nameLeague.description
+        txtLocation.text = nameLeague.location
+
+    }
+    companion object {
+        const val EXTRA_KEY = "extra_key"
     }
 }
