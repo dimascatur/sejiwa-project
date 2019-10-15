b236097798166ebef476a6783e2fbacb21a9bec7
dimascatur48 <dimascatur48@gmail.com>
2019-10-15 15:31:48 +0700

    replace variable

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/League.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/League.kt
@@ -7,9 +7,5 @@ import kotlinx.android.parcel.Parcelize
 data class League(
     var name: String = "",
     var description: String = "",
-    var stadium: String = "",
-    var coach: String = "",
-    var profileCoach: Int = R.drawable.arsenal_coach,
-    var logo: Int = R.drawable.arsenal_logo,
-    var background: Int = R.drawable.arsenal_background
+    var location: String = ""
 ) : Parcelable
