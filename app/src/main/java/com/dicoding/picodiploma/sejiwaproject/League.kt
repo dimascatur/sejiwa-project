2bdda3b599458f60f2d169f4162c566dd6ee83be
dimascatur48 <dimascatur48@gmail.com>
2019-10-15 15:47:35 +0700

    add some variable

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/League.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/League.kt
@@ -7,5 +7,6 @@ import kotlinx.android.parcel.Parcelize
 data class League(
     var name: String = "",
     var description: String = "",
-    var location: String = ""
+    var location: String = "",
+    var photo: Int = 0
 ) : Parcelable
