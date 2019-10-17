4af1a348cbebce4666025e02446e74aa2db41887
dimascatur48 <dimascatur48@gmail.com>
2019-10-17 13:56:23 +0700

    set value as nullable properties

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/model/League.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/model/League.kt
@@ -5,8 +5,8 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class League(
-    var name: String = "",
-    var description: String = "",
-    var location: String = "",
-    var photo: Int = 0
+    var name: String? = "",
+    var description: String? = "",
+    var location: String?= "",
+    var photo: Int? = 0
 ) : Parcelable
