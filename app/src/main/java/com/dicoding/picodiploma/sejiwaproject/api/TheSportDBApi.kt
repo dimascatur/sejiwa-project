a521114ec62f5003afd410c579cb42aadf382e06
dimascatur48 <dimascatur48@gmail.com>
2019-10-22 15:53:22 +0700

    create url class

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/api/TheSportDBApi.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/api/TheSportDBApi.kt
@@ -1,2 +1,18 @@
 package com.dicoding.picodiploma.sejiwaproject.api
 
+import android.net.Uri
+import com.dicoding.picodiploma.sejiwaproject.BuildConfig
+
+object TheSportDBApi {
+    fun getTeams(league: String?): String {
+        return Uri.parse(BuildConfig.BASE_URL).buildUpon()
+            .appendPath("api")
+            .appendPath("v1")
+            .appendPath("json")
+            .appendPath(BuildConfig.TSDB_API_KEY)
+            .appendPath("search_all_teams.php")
+            .appendQueryParameter("l", league)
+            .build()
+            .toString()
+    }
+}
