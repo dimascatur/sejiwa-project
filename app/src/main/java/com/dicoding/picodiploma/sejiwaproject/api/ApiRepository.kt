f24042aadc4a3d5dea7bb90adfbcdf9001e5e28a
dimascatur48 <dimascatur48@gmail.com>
2019-10-22 15:50:56 +0700

    create repository class

---
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/api/ApiRepository.kt
@@ -0,0 +1,9 @@
+package com.dicoding.picodiploma.sejiwaproject.api
+
+import java.net.URL
+
+class ApiRepository {
+    fun doRequest(url: String): String {
+        return URL(url).readText()
+    }
+}
