50daa8fff013bffd72140f62805d34388aa565dc
dimascatur48 <dimascatur48@gmail.com>
2019-10-24 19:41:05 +0700

    nothing change

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/detail/DetailPresenter.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/detail/DetailPresenter.kt
@@ -1,2 +1,28 @@
 package com.dicoding.picodiploma.sejiwaproject.detail
 
+import com.dicoding.picodiploma.sejiwaproject.api.ApiRepository
+import com.dicoding.picodiploma.sejiwaproject.api.TheSportDBApi
+import com.dicoding.picodiploma.sejiwaproject.model.LeaguesResponse
+import com.google.gson.Gson
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+
+class DetailPresenter (private val view: DetailView,
+                       private val apiRepository: ApiRepository,
+                       private val gson: Gson) {
+    fun getDetailList(id: String?) {
+        view.showLoading()
+        doAsync {
+            val data = gson.fromJson(apiRepository
+                .doRequest(TheSportDBApi.getDetails(id)),
+                LeaguesResponse::class.java
+            )
+
+            uiThread {
+                view.hideLoading()
+                view.showDetailList(data.leagues)
+            }
+        }
+    }
+
+}
