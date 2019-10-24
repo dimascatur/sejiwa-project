709e6ff40b4c2342f172a52d0a638d7f705e3ce5
dimascatur48 <dimascatur48@gmail.com>
2019-10-24 10:11:40 +0700

    create some interface

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/detail/DetailView.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/detail/DetailView.kt
@@ -1,2 +1,9 @@
 package com.dicoding.picodiploma.sejiwaproject.detail
 
+import com.dicoding.picodiploma.sejiwaproject.model.Leagues
+
+interface DetailView {
+    fun showLoading()
+    fun hideLoading()
+    fun showDetailList(data: List<Leagues>)
+}
