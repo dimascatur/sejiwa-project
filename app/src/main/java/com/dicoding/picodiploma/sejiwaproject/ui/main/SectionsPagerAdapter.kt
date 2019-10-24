c95a1722c674537a07383f0d8d693bd8ef0df62f
dimascatur48 <dimascatur48@gmail.com>
2019-10-24 19:46:45 +0700

    move this class to the package

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/ui/main/SectionsPagerAdapter.kt
@@ -1,4 +1,4 @@
-package com.dicoding.picodiploma.sejiwaproject.detail.ui.main
+package com.dicoding.picodiploma.sejiwaproject.ui.main
 
 import android.content.Context
 import androidx.fragment.app.Fragment
@@ -7,8 +7,8 @@ import androidx.fragment.app.FragmentPagerAdapter
 import com.dicoding.picodiploma.sejiwaproject.R
 
 private val TAB_TITLES = arrayOf(
-    R.string.tab_text_1,
-    R.string.tab_text_2
+    R.string.tab_1,
+    R.string.tab_2
 )
 
 /**
@@ -20,16 +20,15 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
 
     override fun getItem(position: Int): Fragment {
         // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1)
+        // Return a DetailLeagueFragment (defined as a static inner class below).
+        return DetailLeagueFragment.newInstance(
+            position + 1
+        )
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
         return context.resources.getString(TAB_TITLES[position])
     }
 
-    override fun getCount(): Int {
-        // Show 2 total pages.
-        return 2
+    override fun getCount(): Int = 2
     }
-}
