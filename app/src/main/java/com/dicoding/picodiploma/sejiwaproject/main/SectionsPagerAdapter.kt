fa96ed81db822d8b4fd7f1b10291350988e7cbcf
dimascatur48 <dimascatur48@gmail.com>
2019-10-28 18:12:28 +0700

    create populate fragment function

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/main/SectionsPagerAdapter.kt
@@ -1,11 +1,9 @@
-package com.dicoding.picodiploma.sejiwaproject.ui.main
+package com.dicoding.picodiploma.sejiwaproject.main
 
-import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.dicoding.picodiploma.sejiwaproject.R
-import com.dicoding.picodiploma.sejiwaproject.ui.match.MatchFragment
 
 private val TAB_TITLES = arrayOf(
     R.string.tab_1,
@@ -16,20 +14,18 @@ private val TAB_TITLES = arrayOf(
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
  * one of the sections/tabs/pages.
  */
-class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
-    FragmentPagerAdapter(fm) {
+class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
-    override fun getItem(position: Int): Fragment {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a MatchFragment (defined as a static inner class below).
-        return MatchFragment.newInstance(
-            position + 1
-        )
+    val fragmentList = arrayListOf<Fragment>()
+    val fragmentTitle = arrayListOf<String>()
+
+    fun populateFragment(fragment: Fragment, title: String) {
+        fragmentList.add(fragment)
+        fragmentTitle.add(title)
     }
+    override fun getItem(position: Int): Fragment = fragmentList[position]
 
-    override fun getPageTitle(position: Int): CharSequence? {
-        return context.resources.getString(TAB_TITLES[position])
-    }
+    override fun getCount(): Int = fragmentList.size
 
-    override fun getCount(): Int = 2
+    override fun getPageTitle(position: Int): CharSequence = fragmentTitle[position]
     }
