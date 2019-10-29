019a4a1503facdcfea99acd034fa8cafe8a417e4
dimascatur48 <dimascatur48@gmail.com>
2019-10-29 14:12:41 +0700

    create some data for match detail

---
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/model/detailMatch/DetailMatch.kt
@@ -0,0 +1,41 @@
+package com.dicoding.picodiploma.sejiwaproject.model.detailMatch
+
+import com.google.gson.annotations.SerializedName
+
+data class DetailMatch(
+    @SerializedName("idEvent")
+    var matchId: String? = null,
+
+    @SerializedName("strEvent")
+    var matchTitle: String? = null,
+
+    @SerializedName("strHomeTeam")
+    var teamHome: String? = null,
+
+    @SerializedName("strAwayTeam")
+    var teamAway: String? = null,
+
+    @SerializedName("intHomeScore")
+    var homeScore: String? = null,
+
+    @SerializedName("intAwayScore")
+    var awayScore: String? = null,
+
+    @SerializedName("strHomeGoalDetails")
+    var homeGoal: String? = null,
+
+    @SerializedName("strAwayGoalDetails")
+    var awayGoal: String? = null,
+
+    @SerializedName("strHomeRedCards")
+    var homeRedCard: String? = null,
+
+    @SerializedName("strAwayRedCards")
+    var awayRedCard: String? = null,
+
+    @SerializedName("strHomeYellowCards")
+    var homeYellowCard: String? = null,
+
+    @SerializedName("strAwayYellowCards")
+    var awayYellowCard: String? = null
+)
---
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/model/detailMatch/DetailMatchResponse.kt
@@ -0,0 +1,6 @@
+package com.dicoding.picodiploma.sejiwaproject.model.detailMatch
+
+data class DetailMatchResponse(
+    val events: List<DetailMatch>
+
+)
