29af0caa361ff2d99767a61a8e968e133b68ac7a
dimascatur48 <dimascatur48@gmail.com>
2019-10-29 09:56:27 +0700

    create some uriBuilder function

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/api/TheSportDBApi.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/api/TheSportDBApi.kt
@@ -40,4 +40,15 @@ object TheSportDBApi {
             .toString()
     }
 
+    fun getMatchDetail(idEvent: String?): String {
+        return Uri.parse(BuildConfig.BASE_URL).buildUpon()
+            .appendPath("api")
+            .appendPath("v1")
+            .appendPath("json")
+            .appendPath(BuildConfig.TSDB_API_KEY)
+            .appendPath("lookupevent.php")
+            .appendQueryParameter("id", idEvent)
+            .build()
+            .toString()
+    }
 }
