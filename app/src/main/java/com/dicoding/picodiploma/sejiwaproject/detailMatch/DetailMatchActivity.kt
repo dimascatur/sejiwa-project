eaf217a59a3dada78dd245e5e6d9a320b7c094f1
dimascatur48 <dimascatur48@gmail.com>
2019-10-29 13:53:18 +0700

    add some new string

---
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/detailMatch/DetailMatchActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.picodiploma.sejiwaproject.detailMatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailMatchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_match)
+    }
+}
