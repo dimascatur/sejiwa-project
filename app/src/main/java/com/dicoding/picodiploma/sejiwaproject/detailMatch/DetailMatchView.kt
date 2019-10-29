269c8fb0e4bff951bcdf78bec2e43704be2195e1
dimascatur48 <dimascatur48@gmail.com>
2019-10-29 14:26:27 +0700

    create interface class for match detail

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/detailMatch/DetailMatchView.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/detailMatch/DetailMatchView.kt
@@ -1,2 +1,9 @@
 package com.dicoding.picodiploma.sejiwaproject.detailMatch
 
+import com.dicoding.picodiploma.sejiwaproject.model.detailMatch.DetailMatch
+
+interface DetailMatchView {
+    fun showLoading()
+    fun hideLoading()
+    fun showMatchDetail(data: List<DetailMatch>)
+}
