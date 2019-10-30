1bded0d1a27f07eea1ee2f3aebe29b9518761bda
dimascatur48 <dimascatur48@gmail.com>
2019-10-30 19:49:25 +0700

    create data class for detail team

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/model/detailTeam/DetailTeam.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/model/detailTeam/DetailTeam.kt
@@ -1,2 +1,11 @@
 package com.dicoding.picodiploma.sejiwaproject.model.detailTeam
 
+import com.google.gson.annotations.SerializedName
+
+data class DetailTeam(
+    @SerializedName("idTeam")
+    var idTeam: String? = null,
+
+    @SerializedName("strTeamBadge")
+    var teamLogo: String? = null
+)
