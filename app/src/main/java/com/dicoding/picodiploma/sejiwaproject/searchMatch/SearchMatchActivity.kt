fa5498e2d63854404286cb4bdcaacfca2374b543
dimascatur48 <dimascatur48@gmail.com>
2019-10-30 16:31:34 +0700

    change some color

---
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/searchMatch/SearchMatchActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.picodiploma.sejiwaproject.searchMatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchMatchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_match)
+    }
+}
