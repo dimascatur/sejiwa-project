5c8f225746cddc98581fb075c2d7a12628414399
dimascatur48 <dimascatur48@gmail.com>
2019-10-30 19:51:20 +0700

    add some variable

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/model/nextMatch/NextMatch.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/model/nextMatch/NextMatch.kt
@@ -9,12 +9,18 @@ data class NextMatch(
     @SerializedName("idEvent")
     var matchId: String? = null,
 
-    @SerializedName("strEvent")
-    var matchTitle: String? = null,
-
     @SerializedName("strHomeTeam")
     var teamHome: String? = null,
 
+    @SerializedName("idHomeTeam")
+    var homeId: String? = null,
+
+    @SerializedName("idAwayTeam")
+    var awayId: String? = null,
+
     @SerializedName("strAwayTeam")
-    var teamAway: String? = null
+    var teamAway: String? = null,
+
+    @SerializedName("dateEvent")
+    var dateMatch: String? = null
 ) :Parcelable
