189bba1753115efe2d1eb5678d829ccf595f45a3
dimascatur48 <dimascatur48@gmail.com>
2019-10-30 19:35:32 +0700

    add some function to show logo in activity

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/detailMatch/DetailMatchView.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/detailMatch/DetailMatchView.kt
@@ -1,9 +1,12 @@
 package com.dicoding.picodiploma.sejiwaproject.detailMatch
 
 import com.dicoding.picodiploma.sejiwaproject.model.detailMatch.DetailMatch
+import com.dicoding.picodiploma.sejiwaproject.model.detailTeam.DetailTeam
 
 interface DetailMatchView {
     fun showLoading()
     fun hideLoading()
     fun showMatchDetail(data: List<DetailMatch>)
+    fun showHomeLogo(data: List<DetailTeam>)
+    fun showAwayLogo(data: List<DetailTeam>)
 }
