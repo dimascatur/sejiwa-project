8819a704f558c8d1588886ed4e47ce78a5ea6f20
dimascatur48 <dimascatur48@gmail.com>
2019-10-30 19:43:07 +0700

    implement intent

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/match/nextMatch/NextMatchAdapter.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/match/nextMatch/NextMatchAdapter.kt
@@ -6,7 +6,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.picodiploma.sejiwaproject.R
+import com.dicoding.picodiploma.sejiwaproject.detailMatch.DetailMatchActivity
+import com.dicoding.picodiploma.sejiwaproject.detailMatch.DetailMatchActivity.Companion.EXTRA_ID
 import com.dicoding.picodiploma.sejiwaproject.model.nextMatch.NextMatch
+import org.jetbrains.anko.startActivity
+
 
 class NextMatchAdapter(private val list: List<NextMatch>) :
     RecyclerView.Adapter<NextMatchAdapter.MatchViewHolder>() {
@@ -19,16 +23,20 @@ class NextMatchAdapter(private val list: List<NextMatch>) :
 
     override fun onBindViewHolder(holder: MatchViewHolder, position: Int) {
         val match = list[position]
-        holder.matchTitle.text = match.matchTitle
         holder.homeTeam.text = match.teamHome
         holder.awayTeam.text = match.teamAway
+        holder.dateMatch.text = match.dateMatch
+        holder.itemView.setOnClickListener {
+            it.context.startActivity<DetailMatchActivity>(EXTRA_ID to match.matchId)
+
+        }
     }
 
 
     inner class MatchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val matchTitle: TextView = itemView.findViewById(R.id.match_title)
         val homeTeam: TextView = itemView.findViewById(R.id.team_home)
         val awayTeam: TextView = itemView.findViewById(R.id.team_away)
+        val dateMatch: TextView = itemView.findViewById(R.id.date_match)
     }
 }
 
