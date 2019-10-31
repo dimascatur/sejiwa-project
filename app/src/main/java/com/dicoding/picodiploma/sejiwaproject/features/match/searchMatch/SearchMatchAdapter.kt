68db81e2fc8737e98797bbb0b74f64bb2f812076
dimascatur48 <dimascatur48@gmail.com>
2019-10-31 14:33:39 +0700

    implement intent to detail match class

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/match/searchMatch/SearchMatchAdapter.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/match/searchMatch/SearchMatchAdapter.kt
@@ -6,7 +6,10 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.picodiploma.sejiwaproject.R
+import com.dicoding.picodiploma.sejiwaproject.features.match.detailMatch.DetailMatchActivity
+import com.dicoding.picodiploma.sejiwaproject.features.match.detailMatch.DetailMatchActivity.Companion.EXTRA_ID
 import com.dicoding.picodiploma.sejiwaproject.features.match.searchMatch.model.SearchMatch
+import org.jetbrains.anko.startActivity
 
 class SearchMatchAdapter(private val list: List<SearchMatch>) : RecyclerView.Adapter<SearchMatchAdapter.MatchViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MatchViewHolder {
@@ -20,6 +23,9 @@ class SearchMatchAdapter(private val list: List<SearchMatch>) : RecyclerView.Ada
         holder.homeTeam.text = match.teamHome
         holder.awayTeam.text = match.teamAway
         holder.dateMatch.text = match.dateMatch
+        holder.itemView.setOnClickListener {
+            it.context.startActivity<DetailMatchActivity>(EXTRA_ID to match.matchId)
+        }
     }
 
     inner class MatchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
