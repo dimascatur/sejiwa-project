ee35812802168f35b31c8602a3530eca04463292
dimascatur48 <dimascatur48@gmail.com>
2019-11-01 14:42:14 +0700

    rename parameter

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/commons/api/TheSportDBApi.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/commons/api/TheSportDBApi.kt
@@ -4,14 +4,14 @@ import android.net.Uri
 import com.dicoding.picodiploma.sejiwaproject.BuildConfig
 
 object TheSportDBApi {
-    fun getDetails(idleague: String?): String {
+    fun getDetails(idLeague: String?): String {
         return Uri.parse(BuildConfig.BASE_URL).buildUpon()
             .appendPath("api")
             .appendPath("v1")
             .appendPath("json")
             .appendPath(BuildConfig.TSDB_API_KEY)
             .appendPath("lookupleague.php")
-            .appendQueryParameter("id", idleague)
+            .appendQueryParameter("id", idLeague)
             .build()
             .toString()
     }
