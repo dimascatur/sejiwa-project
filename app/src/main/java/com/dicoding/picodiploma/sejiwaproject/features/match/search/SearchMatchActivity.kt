24623983f27e103930fc1a8d3d75e37637b8029c
dimascatur48 <dimascatur48@gmail.com>
2019-11-01 14:46:47 +0700

    optimized import

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/match/search/SearchMatchActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/match/search/SearchMatchActivity.kt
@@ -4,7 +4,6 @@ import android.app.SearchManager
 import android.content.Context
 import android.os.Bundle
 import android.view.Menu
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
