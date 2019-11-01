67a7f3de5706ddd0e778c3fff997416e5e8b12a5
dimascatur48 <dimascatur48@gmail.com>
2019-11-01 10:06:22 +0700

    remove unnecessary variable and clean code

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/match/next/NextMatchFragment.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/match/next/NextMatchFragment.kt
@@ -6,15 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.picodiploma.sejiwaproject.R
 import com.dicoding.picodiploma.sejiwaproject.commons.api.ApiRepository
 import com.dicoding.picodiploma.sejiwaproject.commons.utils.invisible
 import com.dicoding.picodiploma.sejiwaproject.commons.utils.visible
-import com.dicoding.picodiploma.sejiwaproject.features.league.detail.page.PageViewModel
 import com.dicoding.picodiploma.sejiwaproject.features.match.next.model.NextMatch
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.fragment_next_match.*
@@ -25,23 +22,14 @@ import kotlinx.android.synthetic.main.fragment_next_match.*
 class NextMatchFragment : Fragment(),
     NextMatchView {
     private lateinit var rvMatch: RecyclerView
-    private lateinit var pageViewModel: PageViewModel
     private lateinit var presenter: NextMatchPresenter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        pageViewModel = ViewModelProviders.of(this).get(PageViewModel::class.java).apply {
-            setIndex(arguments?.getInt(ARG_SECTION_NUMBER) ?: 2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_next_match, container, false)
-        pageViewModel.text.observe(this, Observer<String> {})
-        return root
+        return inflater.inflate(R.layout.fragment_next_match, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -66,7 +54,6 @@ class NextMatchFragment : Fragment(),
 
     companion object {
 
-        private const val ARG_SECTION_NUMBER = "section_number"
         private const val ID_LEAGUE = "id"
 
         @JvmStatic
