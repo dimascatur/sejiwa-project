1b81028c09e00122be24409e4b460d65ee3117a3
dimascatur48 <dimascatur48@gmail.com>
2019-11-04 16:48:55 +0700

    create presenter class for team detail

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/team/DetailTeamPresenter.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/team/DetailTeamPresenter.kt
@@ -1,2 +1,25 @@
 package com.dicoding.picodiploma.sejiwaproject.features.team
 
+import com.dicoding.picodiploma.sejiwaproject.commons.api.ApiRepository
+import com.dicoding.picodiploma.sejiwaproject.commons.api.TheSportDBApi
+import com.dicoding.picodiploma.sejiwaproject.features.team.model.TeamResponse
+import com.google.gson.Gson
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+
+class DetailTeamPresenter(private val view: DetailTeamView,
+                          private val apiRepository: ApiRepository,
+                          private val gson: Gson) {
+
+    fun getDetailTeam(id: String?){
+        doAsync {
+            val data = gson.fromJson(apiRepository
+                .doRequest(TheSportDBApi.getTeamDetail(id)),
+                TeamResponse::class.java
+            )
+            uiThread {
+                view.showDetailTeam(data.teams.first())
+            }
+        }
+    }
+}
