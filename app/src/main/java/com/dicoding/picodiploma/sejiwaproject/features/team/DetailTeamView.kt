8e228ac72d025b032220d6b8d13c6527c98071c4
dimascatur48 <dimascatur48@gmail.com>
2019-11-04 16:46:28 +0700

    create interface for team detail

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/team/DetailTeamView.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/team/DetailTeamView.kt
@@ -1,2 +1,7 @@
 package com.dicoding.picodiploma.sejiwaproject.features.team
 
+import com.dicoding.picodiploma.sejiwaproject.features.team.model.Team
+
+interface DetailTeamView {
+    fun showDetailTeam(data: Team)
+}
