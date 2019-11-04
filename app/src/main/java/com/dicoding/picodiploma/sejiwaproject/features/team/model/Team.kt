dc7a985b00982e546fd0546c156cbd2050bf77cd
dimascatur48 <dimascatur48@gmail.com>
2019-11-04 16:04:13 +0700

    add some variable

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/team/model/Team.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/team/model/Team.kt
@@ -2,9 +2,15 @@ package com.dicoding.picodiploma.sejiwaproject.features.team.model
 
 import com.google.gson.annotations.SerializedName
 
-data class LogoTeam(
+data class Team(
     @SerializedName("idTeam")
     var idTeam: String? = null,
+
+    @SerializedName("strTeam")
+    var teamName: String? = null,
+
+    @SerializedName("strStadiumThumb")
+    var teamStadium: String? = null,
 
     @SerializedName("strTeamBadge")
     var teamLogo: String? = null
