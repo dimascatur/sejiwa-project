294a25e9aae18997e2e18b66766d87b672811253
dimascatur48 <dimascatur48@gmail.com>
2019-11-04 16:03:44 +0700

    move this class to another package

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/team/model/TeamResponse.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/team/model/TeamResponse.kt
@@ -1,7 +1,5 @@
 package com.dicoding.picodiploma.sejiwaproject.features.team.model
 
-import com.dicoding.picodiploma.sejiwaproject.features.team.model.LogoTeam
-
-data class LogoTeamResponse(
-    val teams: List<LogoTeam>
+data class TeamResponse(
+    val teams: List<Team>
 )
