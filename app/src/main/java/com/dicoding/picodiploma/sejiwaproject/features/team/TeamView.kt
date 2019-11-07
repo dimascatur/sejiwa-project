2b808825132cb604c02235fda65e35ee2aa7a22b
dimascatur48 <dimascatur48@gmail.com>
2019-11-07 10:22:56 +0700

    create interface for team

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/team/TeamView.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/team/TeamView.kt
@@ -2,6 +2,8 @@ package com.dicoding.picodiploma.sejiwaproject.features.team
 
 import com.dicoding.picodiploma.sejiwaproject.features.team.model.Team
 
-interface DetailTeamView {
+interface TeamView {
+    fun showLoading()
+    fun hideLoading()
     fun showDetailTeam(data: Team)
 }
