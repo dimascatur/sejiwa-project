31e4fed19280e7facacda76ee6c47ee50f43fdf1
dimascatur48 <dimascatur48@gmail.com>
2019-11-08 14:12:05 +0700

    create data class for database open helper

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/db/Favorite.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/db/Favorite.kt
@@ -1,10 +1,13 @@
-package com.dicoding.picodiploma.sejiwaproject.features.favorite
+package com.dicoding.picodiploma.sejiwaproject.db
 
-data class Favorite(val matchId: String?, val teamHome: String?, val homeId: String?,
-                    val awayId: String?, val teamAway: String?, val dateMatch: String?,
-                    val badgeHome: String?, val badgeAway: String?) {
+data class Favorite(val matchId: String?, val teamHome: String?,
+                    val teamAway: String?, val dateMatch: String?) {
 
     companion object{
-        const val
+        const val TABLE_FAVORITE: String = "TABLE_FAVORITE"
+        const val MATCH_ID: String = "MATCH_ID"
+        const val TEAM_HOME: String = "TEAM_HOME"
+        const val TEAM_AWAY: String = "TEAM_AWAY"
+        const val DATE_MATCH: String = "DATE_MATCH"
     }
 }
