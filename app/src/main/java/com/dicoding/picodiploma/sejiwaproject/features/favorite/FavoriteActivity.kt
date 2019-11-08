3488206bac2570d326cce56b769a1fe66a0b4af1
dimascatur48 <dimascatur48@gmail.com>
2019-11-08 14:10:26 +0700

    create activity  to show favorite match

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/favorite/FavoriteActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/favorite/FavoriteActivity.kt
@@ -1,12 +1,41 @@
 package com.dicoding.picodiploma.sejiwaproject.features.favorite
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.picodiploma.sejiwaproject.R
+import com.dicoding.picodiploma.sejiwaproject.features.league.detail.DetailPagerAdapter
+import kotlinx.android.synthetic.main.activity_favorite.*
 
 class FavoriteActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorite)
+
+        val teamMatch = intent.getStringExtra(EXTRA_ID)
+
+
+        val sectionsPagerAdapter =
+            DetailPagerAdapter(
+                supportFragmentManager
+            )
+
+        sectionsPagerAdapter.populateFragment(
+            Fragment.newInstance(
+                teamMatch ?: "68890"
+            ), "Match"
+        )
+        view_pager_favorite.adapter = sectionsPagerAdapter
+        tabs.setupWithViewPager(view_pager_favorite)
+
+        favorite_toolbar.title = "Favorite Match"
+
+        collapsing.setExpandedTitleColor(Color.TRANSPARENT)
+        collapsing.setCollapsedTitleTextColor(Color.WHITE)
+    }
+
+    companion object {
+        const val EXTRA_ID = "extra_id"
     }
 }
