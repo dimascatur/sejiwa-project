2825039089c40cfa0057b657242a583edf13f44c
dimascatur48 <dimascatur48@gmail.com>
2019-11-08 14:08:43 +0700

    implement on create options menu for intent to favorite activity

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/league/list/LeagueListActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/league/list/LeagueListActivity.kt
@@ -1,10 +1,14 @@
 package com.dicoding.picodiploma.sejiwaproject.features.league.list
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.picodiploma.sejiwaproject.R
+import com.dicoding.picodiploma.sejiwaproject.features.favorite.FavoriteActivity
 
 class LeagueListActivity : AppCompatActivity() {
     private lateinit var rvLeague: RecyclerView
@@ -46,6 +50,21 @@ class LeagueListActivity : AppCompatActivity() {
             )
         }
         logo.recycle()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.favorite_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.favorite -> {
+                val moveIntent = Intent(this@LeagueListActivity, FavoriteActivity::class.java)
+                startActivity(moveIntent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
 
