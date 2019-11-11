db817496d0abc36f8161782d8a8dafcee9c4ef2c
dimascatur48 <dimascatur48@gmail.com>
2019-11-11 14:48:07 +0700

    implement some functional code to get logo

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/match/detail/DetailMatchPresenter.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/match/detail/DetailMatchPresenter.kt
@@ -8,44 +8,49 @@ import com.google.gson.Gson
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
-class DetailMatchPresenter (private var view: DetailMatchView?,
-                            private val apiRepository: ApiRepository,
-                            private val gson: Gson) {
+class DetailMatchPresenter(
+    private var view: DetailMatchView?,
+    private val apiRepository: ApiRepository,
+    private val gson: Gson
+) {
 
-    fun onDetach(){
+    fun onDetach() {
         view = null
     }
 
     fun getDetailMatch(id: String?) {
         view?.showLoading()
         doAsync {
-            val data = gson.fromJson(apiRepository
-                .doRequest(TheSportDBApi.getMatchDetails(id)),
+            val data = gson.fromJson(
+                apiRepository
+                    .doRequest(TheSportDBApi.getMatchDetails(id)),
                 DetailMatchResponse::class.java
             )
-            uiThread {
-                view?.hideLoading()
-                view?.showMatchDetail(data.events)
-            }
-        }
-    }
-    fun getTeamLogo(id: String?, isHome: Boolean){
-            view?.showLoading()
-            doAsync {
-                val data = gson.fromJson(apiRepository
-                    .doRequest(TheSportDBApi.getTeamDetail(id)),
+
+            data.events.map {
+                val homeResponse = gson.fromJson(
+                    apiRepository
+                        .doRequest(TheSportDBApi.getTeamDetail(it.homeId)),
                     TeamResponse::class.java
                 )
-                uiThread {
-                    if (isHome){
-                        view?.hideLoading()
-                        view?.showHomeLogo(data.teams)
-                    } else {
-                        view?.hideLoading()
-                        view?.showAwayLogo(data.teams)
-                    }
 
+                val awayResponse = gson.fromJson(
+                    apiRepository
+                        .doRequest(TheSportDBApi.getTeamDetail(it.awayId)),
+                    TeamResponse::class.java
+                )
+
+                val result = it.copy()
+                result.awayId = awayResponse.teams.first().idTeam
+                result.homeId = homeResponse.teams.first().idTeam
+                result.badgeHome = homeResponse.teams.first().teamLogo
+                result.badgeAway = awayResponse.teams.first().teamLogo
+
+                uiThread {
+                    view?.hideLoading()
+                    view?.matchReady(result)
                 }
             }
+        }
     }
 }
