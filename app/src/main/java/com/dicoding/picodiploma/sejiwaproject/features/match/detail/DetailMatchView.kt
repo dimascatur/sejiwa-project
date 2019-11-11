ea9d7fe45fa0ccd5675e391de36ac3946620dbaf
dimascatur48 <dimascatur48@gmail.com>
2019-11-11 14:47:14 +0700

    add some function

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/match/detail/DetailMatchView.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/match/detail/DetailMatchView.kt
@@ -1,12 +1,10 @@
 package com.dicoding.picodiploma.sejiwaproject.features.match.detail
 
 import com.dicoding.picodiploma.sejiwaproject.features.match.detail.model.DetailMatch
-import com.dicoding.picodiploma.sejiwaproject.features.team.model.Team
 
 interface DetailMatchView {
     fun showLoading()
     fun hideLoading()
-    fun showMatchDetail(data: List<DetailMatch>)
-    fun showHomeLogo(data: List<Team>)
-    fun showAwayLogo(data: List<Team>)
+    fun matchReady(detailMatch: DetailMatch)
+
 }
