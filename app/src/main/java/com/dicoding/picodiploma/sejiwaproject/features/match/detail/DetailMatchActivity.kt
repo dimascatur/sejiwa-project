39000d3878a052f1962759f5c4673362431f7c0e
dimascatur48 <dimascatur48@gmail.com>
2019-11-11 14:48:53 +0700

    move init data to new function

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/match/detail/DetailMatchActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/match/detail/DetailMatchActivity.kt
@@ -14,7 +14,6 @@ import com.dicoding.picodiploma.sejiwaproject.commons.utils.visible
 import com.dicoding.picodiploma.sejiwaproject.db.Favorite
 import com.dicoding.picodiploma.sejiwaproject.db.database
 import com.dicoding.picodiploma.sejiwaproject.features.match.detail.model.DetailMatch
-import com.dicoding.picodiploma.sejiwaproject.features.team.model.Team
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_detail_match.*
 import org.jetbrains.anko.db.classParser
@@ -25,7 +24,6 @@ import org.jetbrains.anko.db.select
 class DetailMatchActivity : AppCompatActivity(),
     DetailMatchView {
     private lateinit var match: DetailMatch
-    private var matches: MutableList<DetailMatch> = mutableListOf()
     private lateinit var presenter: DetailMatchPresenter
     private var isFavorite: Boolean = false
     private var menuItem: Menu? = null
@@ -63,48 +61,6 @@ class DetailMatchActivity : AppCompatActivity(),
         progress_bar.invisible()
     }
 
-    override fun showMatchDetail(data: List<DetailMatch>) {
-        if (data[0].matchTitle.isNullOrEmpty()) {
-            home_score.invisible()
-            away_score.invisible()
-            home_goal.invisible()
-            away_goal.invisible()
-            home_red.invisible()
-            away_red.invisible()
-            home_yellow.invisible()
-            away_yellow.invisible()
-            date_match.invisible()
-
-        } else {
-            match = DetailMatch(
-                matchId = data[0].matchId,
-                matchTitle = data[0].matchTitle,
-                teamHome = data[0].teamHome,
-                teamAway = data[0].teamAway,
-                dateMatch = data[0].dateMatch
-            )
-
-            home_score.text = data[0].homeScore
-            away_score.text = data[0].awayScore
-            home_goal.text = data[0].homeGoal
-            away_goal.text = data[0].awayGoal
-            home_red.text = data[0].homeRedCard
-            away_red.text = data[0].awayRedCard
-            home_yellow.text = data[0].homeYellowCard
-            away_yellow.text = data[0].awayYellowCard
-            date_match.text = data[0].dateMatch
-        }
-        match_title.text = data[0].matchTitle
-        team_home.text = data[0].teamHome
-        team_away.text = data[0].teamAway
-
-        matches.clear()
-        matches.addAll(data)
-        presenter.getTeamLogo(data[0].homeId, true)
-        presenter.getTeamLogo(data[0].awayId, false)
-
-    }
-
     private fun favoriteState() {
         database.use {
             val result = select(Favorite.TABLE_FAVORITE)
@@ -117,15 +73,49 @@ class DetailMatchActivity : AppCompatActivity(),
         }
     }
 
-    override fun showHomeLogo(data: List<Team>) {
-        Glide.with(this)
-            .load(data[0].teamLogo)
-            .into(home_logo)
-    }
+    override fun matchReady(detailMatch: DetailMatch) {
+        if (detailMatch.matchTitle.isNullOrEmpty()) {
+            home_score.invisible()
+            away_score.invisible()
+            home_goal.invisible()
+            away_goal.invisible()
+            home_red.invisible()
+            away_red.invisible()
+            home_yellow.invisible()
+            away_yellow.invisible()
+            date_match.invisible()
 
-    override fun showAwayLogo(data: List<Team>) {
+        } else {
+            match = DetailMatch(
+                matchId = detailMatch.matchId,
+                matchTitle = detailMatch.matchTitle,
+                teamHome = detailMatch.teamHome,
+                teamAway = detailMatch.teamAway,
+                dateMatch = detailMatch.dateMatch,
+                badgeHome = detailMatch.badgeHome,
+                badgeAway = detailMatch.badgeAway
+            )
+
+            home_score.text = detailMatch.homeScore
+            away_score.text = detailMatch.awayScore
+            home_goal.text = detailMatch.homeGoal
+            away_goal.text = detailMatch.awayGoal
+            home_red.text = detailMatch.homeRedCard
+            away_red.text = detailMatch.awayRedCard
+            home_yellow.text = detailMatch.homeYellowCard
+            away_yellow.text = detailMatch.awayYellowCard
+            date_match.text = detailMatch.dateMatch
+        }
+        match_title.text = detailMatch.matchTitle
+        team_home.text = detailMatch.teamHome
+        team_away.text = detailMatch.teamAway
+
         Glide.with(this)
-            .load(data[0].teamLogo)
+            .load(detailMatch.badgeHome)
+            .into(home_logo)
+
+        Glide.with(this)
+            .load(detailMatch.badgeAway)
             .into(away_logo)
     }
 
@@ -164,7 +154,9 @@ class DetailMatchActivity : AppCompatActivity(),
                     Favorite.MATCH_ID to match.matchId,
                     Favorite.TEAM_HOME to match.teamHome,
                     Favorite.TEAM_AWAY to match.teamAway,
-                    Favorite.DATE_MATCH to match.dateMatch
+                    Favorite.DATE_MATCH to match.dateMatch,
+                    Favorite.BADGE_HOME to match.badgeHome,
+                    Favorite.BADGE_AWAY to match.badgeAway
                 )
             }
         } catch (e: SQLiteConstraintException) {
