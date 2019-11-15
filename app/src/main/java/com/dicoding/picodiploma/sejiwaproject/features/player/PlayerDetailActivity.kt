fb13cfac23debace1e37258b5b3211af460e3971
dimascatur48 <dimascatur48@gmail.com>
2019-11-15 20:34:41 +0700

    add some string

---
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/player/PlayerDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.picodiploma.sejiwaproject.features.player
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayerDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player_detail)
+    }
+}
