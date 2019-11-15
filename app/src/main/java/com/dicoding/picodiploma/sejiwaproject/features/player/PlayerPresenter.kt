5a6de5704a870c82477f138ac36babada7515977
dimascatur48 <dimascatur48@gmail.com>
2019-11-15 20:38:10 +0700

    add function to get player detail in presenter

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/player/PlayerPresenter.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/player/PlayerPresenter.kt
@@ -2,6 +2,7 @@ package com.dicoding.picodiploma.sejiwaproject.features.player
 
 import com.dicoding.picodiploma.sejiwaproject.commons.api.ApiRepository
 import com.dicoding.picodiploma.sejiwaproject.commons.api.TheSportDBApi
+import com.dicoding.picodiploma.sejiwaproject.features.player.model.PlayerDetailResponse
 import com.dicoding.picodiploma.sejiwaproject.features.player.model.PlayerResponse
 import com.google.gson.Gson
 import org.jetbrains.anko.doAsync
@@ -12,17 +13,31 @@ class PlayerPresenter(
     private val apiRepository: ApiRepository,
     private val gson: Gson
 ) {
-    fun getPlayerGrid(idTeam: String) {
+    fun getPlayerLogo(idTeam: String) {
         view.showLoading()
         doAsync {
             val data = gson.fromJson(
                 apiRepository
-                    .doRequest(TheSportDBApi.getPlayerDetail(idTeam)),
+                    .doRequest(TheSportDBApi.getPlayerLogo(idTeam)),
                 PlayerResponse::class.java
             )
             uiThread {
                 view.hideLoading()
-                view.showPlayerGrid(data.player)
+                view.showPlayerDetail(data.player)
+            }
+        }
+    }
+
+    fun getPlayerDetail(idPlayer: String) {
+        doAsync {
+            val data = gson.fromJson(
+                apiRepository
+                    .doRequest(TheSportDBApi.getPlayerDetail(idPlayer)),
+                PlayerDetailResponse::class.java
+            )
+            uiThread {
+                view.hideLoading()
+                view.showPlayerDetail(data.players)
             }
         }
     }
