284093cb23340daea6eb84bd65e84dd65521947e
dimascatur48 <dimascatur48@gmail.com>
2019-11-18 19:57:11 +0700

    add some library

---
+++ app/src/test/java/com/dicoding/picodiploma/sejiwaproject/features/TestContextProvider.kt
@@ -0,0 +1,4 @@
+package com.dicoding.picodiploma.sejiwaproject.features
+
+class TestContextProvider {
+}
---
+++ app/src/test/java/com/dicoding/picodiploma/sejiwaproject/features/match/next/NextMatchPresenterTest.kt
@@ -0,0 +1,5 @@
+package com.dicoding.picodiploma.sejiwaproject.features.match.next
+
+import org.junit.Assert.*
+
+class NextMatchPresenterTest
