d54e052e0788128b32887cb1e7554cf88ef45f99
dimascatur48 <dimascatur48@gmail.com>
2019-11-19 17:15:50 +0700

    create unit test for search match

--- app/src/test/java/com/dicoding/picodiploma/sejiwaproject/features/match/search/SearchMatchPresenterTest.kt
+++ app/src/test/java/com/dicoding/picodiploma/sejiwaproject/features/match/search/SearchMatchPresenterTest.kt
@@ -1,5 +1,62 @@
 package com.dicoding.picodiploma.sejiwaproject.features.match.search
 
-import org.junit.Assert.*
+import com.dicoding.picodiploma.sejiwaproject.commons.api.ApiRepository
+import com.dicoding.picodiploma.sejiwaproject.features.TestContextProvider
+import com.dicoding.picodiploma.sejiwaproject.features.match.search.model.SearchMatch
+import com.dicoding.picodiploma.sejiwaproject.features.match.search.model.SearchMatchResponse
+import com.google.gson.Gson
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentMatchers
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
 
-class SearchMatchPresenterTest
+class SearchMatchPresenterTest{
+
+    @Mock
+    private lateinit var view: SearchMatchView
+
+    @Mock
+    private lateinit var gson: Gson
+
+    @Mock
+    private lateinit var apiRepository: ApiRepository
+
+    @Mock
+    private lateinit var apiResponse: Deferred<String>
+
+    private lateinit var presenter: SearchMatchPresenter
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        presenter = SearchMatchPresenter(view, apiRepository, gson, TestContextProvider())
+    }
+
+    @Test
+    fun `test get search match is ready`() {
+        val event: MutableList<SearchMatch> = mutableListOf()
+        val response = SearchMatchResponse(event)
+        val idEvent = "610504"
+
+        runBlocking {
+            Mockito.`when`(apiRepository.doRequestAsync(ArgumentMatchers.anyString()))
+                .thenReturn(apiResponse)
+            Mockito.`when`(apiResponse.await()).thenReturn("")
+            Mockito.`when`(
+                gson.fromJson(
+                    "",
+                    SearchMatchResponse::class.java)
+            ).thenReturn(response)
+
+            presenter.getSearchMatch(idEvent)
+
+            Mockito.verify(view).showLoading()
+            Mockito.verify(view).showSearchList(event)
+            Mockito.verify(view).hideLoading()
+        }
+    }
+}
