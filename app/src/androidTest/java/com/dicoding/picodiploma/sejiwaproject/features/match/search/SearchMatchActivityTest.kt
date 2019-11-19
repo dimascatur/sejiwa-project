1b6feec1f4bedc48227cda46fab1bd2dd39e41a8
dimascatur48 <dimascatur48@gmail.com>
2019-11-19 17:19:24 +0700

    create ui test to check search match behavior

--- app/src/androidTest/java/com/dicoding/picodiploma/sejiwaproject/features/match/search/SearchMatchActivityTest.kt
+++ app/src/androidTest/java/com/dicoding/picodiploma/sejiwaproject/features/match/search/SearchMatchActivityTest.kt
@@ -1,5 +1,60 @@
 package com.dicoding.picodiploma.sejiwaproject.features.match.search
 
-import org.junit.Assert.*
+import android.widget.EditText
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.dicoding.picodiploma.sejiwaproject.R
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
-class SearchMatchActivityTest
+
+@RunWith(AndroidJUnit4::class)
+class SearchMatchActivityTest {
+    @Rule
+    @JvmField
+    var activityRule = ActivityTestRule(SearchMatchActivity::class.java)
+
+    @Test
+    fun testSearchMatchWithQueryArsenal() {
+        onView(isAssignableFrom(EditText::class.java)).perform(
+            typeText("Arsenal"),
+            pressImeActionButton()
+        )
+        onView(withId(R.id.rv_search_match)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                1
+            )
+        )
+        onView(withId(R.id.rv_search_match)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(1, click())
+        )
+
+        pressBack()
+    }
+
+    @Test
+    fun testSearchMatchWithQueryChelsea() {
+        onView(isAssignableFrom(EditText::class.java)).perform(
+            typeText("Chelsea"),
+            pressImeActionButton()
+        )
+        onView(withId(R.id.rv_search_match)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                1
+            )
+        )
+        onView(withId(R.id.rv_search_match)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(1, click())
+        )
+
+        pressBack()
+
+    }
+}
