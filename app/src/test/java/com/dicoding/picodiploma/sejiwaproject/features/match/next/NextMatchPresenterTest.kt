ecfddf0b41ae4f7c190e106c15481fe430b9caa0
dimascatur48 <dimascatur48@gmail.com>
2019-11-19 17:16:47 +0700

    create unit test for next match

--- app/src/test/java/com/dicoding/picodiploma/sejiwaproject/features/match/next/NextMatchPresenterTest.kt
+++ app/src/test/java/com/dicoding/picodiploma/sejiwaproject/features/match/next/NextMatchPresenterTest.kt
@@ -4,6 +4,8 @@ import com.dicoding.picodiploma.sejiwaproject.commons.api.ApiRepository
 import com.dicoding.picodiploma.sejiwaproject.features.TestContextProvider
 import com.dicoding.picodiploma.sejiwaproject.features.match.next.model.NextMatch
 import com.dicoding.picodiploma.sejiwaproject.features.match.next.model.NextMatchResponse
+import com.dicoding.picodiploma.sejiwaproject.features.team.model.Team
+import com.dicoding.picodiploma.sejiwaproject.features.team.model.TeamResponse
 import com.google.gson.Gson
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.runBlocking
@@ -15,7 +17,7 @@ import org.mockito.Mockito
 import org.mockito.Mockito.times
 import org.mockito.MockitoAnnotations
 
-class NextMatchPresenterTest{
+class NextMatchPresenterTest {
     @Mock
     private lateinit var view: NextMatchView
 
@@ -38,31 +40,65 @@ class NextMatchPresenterTest{
     }
 
     @Test
-    fun testGetNextMatch() {
-        var matches= NextMatch(
+    fun `test get match is ready`() {
+        val match = NextMatch(
             "602249", "Crystal Palace",
-            "133632", "133602", "Liverpool",
-            "2019-11-23", "crystal logo", "liverpool logo"
+            "133632", "133632", "Liverpool",
+            "2019-11-23"
         )
-        val match: MutableList<NextMatch> = mutableListOf(matches)
-        val response = NextMatchResponse(match)
+
+        val matches: MutableList<NextMatch> = mutableListOf(match)
+        val response = NextMatchResponse(matches)
+
+        val teamHome = Team(
+            idTeam = "133632",
+            teamBadge = "https://www.thesportsdb.com/images/media/team/badge/a1af2i1557005128.png"
+        )
+
+        val teamsHome: MutableList<Team> = mutableListOf(teamHome)
+        val teamHomeResponse = TeamResponse(teamsHome)
+
+        val teamAway = Team(
+            idTeam = "133632",
+            teamBadge = "https://www.thesportsdb.com/images/media/team/badge/a1af2i1557005128.png"
+        )
+
+        val teamsAway: MutableList<Team> = mutableListOf(teamAway)
+        val teamAwayResponse = TeamResponse(teamsAway)
+
         val idLeague = "4328"
 
         runBlocking {
-            Mockito.`when`(apiRepository.doRequestAsync(ArgumentMatchers.anyString())).thenReturn(apiResponse)
-
-            Mockito.`when`(apiResponse.await()).thenReturn("")
+            Mockito.`when`(apiRepository.doRequestAsync(ArgumentMatchers.anyString()))
+                .thenReturn(apiResponse)
+            Mockito.`when`(apiResponse.await())
+                .thenReturn("")
 
             Mockito.`when`(
                 gson.fromJson(
                     "",
-                    NextMatchResponse::class.java)
+                    NextMatchResponse::class.java
+                )
             ).thenReturn(response)
+
+            Mockito.`when`(
+                gson.fromJson(
+                    "",
+                    TeamResponse::class.java
+                )
+            ).thenReturn(teamHomeResponse)
+
+            Mockito.`when`(
+                gson.fromJson(
+                    "",
+                    TeamResponse::class.java
+                )
+            ).thenReturn(teamAwayResponse)
 
             presenter.getNextMatch(idLeague)
 
             Mockito.verify(view).showLoading()
-            Mockito.verify(view, times(1)).matchReady(matches)
+            Mockito.verify(view, times(matches.size)).matchReady(match)
             Mockito.verify(view).hideLoading()
         }
     }
