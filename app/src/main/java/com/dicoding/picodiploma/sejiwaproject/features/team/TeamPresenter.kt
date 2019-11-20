43dd2dcc2407b05fdb48ef5c4b935c624cf190a1
dimascatur48 <dimascatur48@gmail.com>
2019-11-20 17:31:56 +0700

    add some function to set the database

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/team/TeamPresenter.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/team/TeamPresenter.kt
@@ -1,17 +1,26 @@
 package com.dicoding.picodiploma.sejiwaproject.features.team
 
+import android.database.sqlite.SQLiteConstraintException
 import com.dicoding.picodiploma.sejiwaproject.commons.api.ApiRepository
 import com.dicoding.picodiploma.sejiwaproject.commons.api.TheSportDBApi
 import com.dicoding.picodiploma.sejiwaproject.commons.utils.CoroutineContextProvider
+import com.dicoding.picodiploma.sejiwaproject.db.FavoriteTeam
+import com.dicoding.picodiploma.sejiwaproject.db.MyDatabaseOpenHelper
+import com.dicoding.picodiploma.sejiwaproject.features.team.model.Team
 import com.dicoding.picodiploma.sejiwaproject.features.team.model.TeamResponse
 import com.google.gson.Gson
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import org.jetbrains.anko.db.classParser
+import org.jetbrains.anko.db.delete
+import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.db.select
 
 class TeamPresenter(
     private val view: TeamView,
     private val apiRepository: ApiRepository,
     private val gson: Gson,
+    private val database: MyDatabaseOpenHelper,
     private val context: CoroutineContextProvider = CoroutineContextProvider()
 ) {
 
@@ -26,6 +35,44 @@ class TeamPresenter(
 
             view.hideLoading()
             view.showDetailTeam(data.teams.first())
+        }
+    }
+
+    fun favoriteState(teamId: String) {
+        database.use {
+            val result = select(FavoriteTeam.TABLE_FAVORITE)
+                .whereArgs(
+                    "(TEAM_ID = {id})",
+                    "id" to teamId
+                )
+            val favorite = result.parseList(classParser<FavoriteTeam>())
+            if (favorite.isNotEmpty()) view.favoriteState(true)
+        }
+    }
+
+    fun addToFavorite(team: Team) {
+        try {
+            database.use {
+                insert(
+                    FavoriteTeam.TABLE_FAVORITE,
+                    FavoriteTeam.TEAM_ID to team.idTeam,
+                    FavoriteTeam.TEAM_NAME to team.teamName,
+                    FavoriteTeam.TEAM_LOGO to team.teamLogo
+                )
+            }
+        } catch (e: SQLiteConstraintException) {
+        }
+    }
+
+    fun removeFromFavorite(teamId: String) {
+        try {
+            database.use {
+                delete(
+                    FavoriteTeam.TABLE_FAVORITE, "(TEAM_ID = {id})",
+                    "id" to teamId
+                )
+            }
+        } catch (e: SQLiteConstraintException) {
         }
     }
 }
