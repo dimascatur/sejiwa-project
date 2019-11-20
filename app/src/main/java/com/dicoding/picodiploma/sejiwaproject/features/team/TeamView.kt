87f41967527f6fb000631f0151c6f4e7b18e7b33
dimascatur48 <dimascatur48@gmail.com>
2019-11-20 18:33:37 +0700

    remove unnecessary function

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/team/TeamView.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/team/TeamView.kt
@@ -3,8 +3,6 @@ package com.dicoding.picodiploma.sejiwaproject.features.team
 import com.dicoding.picodiploma.sejiwaproject.features.team.model.Team
 
 interface TeamView {
-    fun showLoading()
-    fun hideLoading()
     fun showDetailTeam(data: Team)
     fun favoriteState(isFavorite: Boolean)
 }
