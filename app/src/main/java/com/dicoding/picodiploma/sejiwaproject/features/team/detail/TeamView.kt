d767d4a7c5b1d3c8163a5aae3c4cd03727303c12
dimascatur48 <dimascatur48@gmail.com>
2019-11-20 17:28:30 +0700

    add some function

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/team/detail/TeamView.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/features/team/detail/TeamView.kt
@@ -1,2 +1,9 @@
 package com.dicoding.picodiploma.sejiwaproject.features.team.detail
 
+import com.dicoding.picodiploma.sejiwaproject.features.team.model.Team
+
+interface TeamView {
+    fun showLoading()
+    fun hideLoading()
+    fun showDetailTeam(data: Team)
+}
