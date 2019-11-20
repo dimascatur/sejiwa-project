3d33e3f8539e55c91ab3c930181b7992953957c6
dimascatur48 <dimascatur48@gmail.com>
2019-11-20 17:37:18 +0700

    create data class for favorite team database

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/db/FavoriteTeam.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/db/FavoriteTeam.kt
@@ -1,2 +1,12 @@
 package com.dicoding.picodiploma.sejiwaproject.db
 
+data class FavoriteTeam(val idTeam: String?, val teamName: String?,
+                        val teamBadge: String?) {
+
+    companion object{
+        const val TABLE_FAVORITE: String = "TABLE_TEAM"
+        const val TEAM_ID: String = "TEAM_ID"
+        const val TEAM_NAME: String = "TEAM_NAME"
+        const val TEAM_LOGO: String = "TEAM_LOGO"
+    }
+}
