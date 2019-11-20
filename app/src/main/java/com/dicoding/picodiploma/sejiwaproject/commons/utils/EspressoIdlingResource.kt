9dcecb08582714765f56f9c2f95ef46b1ad19468
dimascatur48 <dimascatur48@gmail.com>
2019-11-20 18:35:59 +0700

    create idlingResource class

--- app/src/main/java/com/dicoding/picodiploma/sejiwaproject/commons/utils/EspressoIdlingResource.kt
+++ app/src/main/java/com/dicoding/picodiploma/sejiwaproject/commons/utils/EspressoIdlingResource.kt
@@ -1,2 +1,20 @@
 package com.dicoding.picodiploma.sejiwaproject.commons.utils
 
+import androidx.test.espresso.IdlingResource
+import androidx.test.espresso.idling.CountingIdlingResource
+
+object EspressoIdlingResource {
+    private val RESOURCE = "GLOBAL"
+    private val countingIdlingResource = CountingIdlingResource(RESOURCE)
+
+    val idlingresource: IdlingResource
+        get() = countingIdlingResource
+
+    fun increment() {
+        countingIdlingResource.increment()
+    }
+
+    fun decrement() {
+        countingIdlingResource.decrement()
+    }
+}
